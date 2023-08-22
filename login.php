35ea8a34241d7e180fae1521da5cba24ae004527
sivavithu <sivavithu15@live.com>
2023-08-22 23:07:53 +0530

    Update login.php

--- login.php
+++ login.php
@@ -89,7 +89,7 @@
                 $_SESSION['user_id'] = $row['user_id'];
                 $_SESSION['role'] = $row['role'];
                ob_end_clean();
-                 header("location:index.php");
+                 include("index.php");
                         exit;
 
                   

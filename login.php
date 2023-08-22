67f95bb56e4078e4f34125d9a59ebcc7e4bd2930
sivavithu <sivavithu15@live.com>
2023-08-22 16:02:23 +0530

    Update login.php

--- login.php
+++ login.php
@@ -46,7 +46,7 @@
             </div>
         </form>
         <div class="forgotpass">
-            <a href="otp.php"><button>forgot password</button></a>
+            <a href="/otp.php"><button>forgot password</button></a>
         </div>
     </div>
 
@@ -61,10 +61,10 @@
     }
     if (isset($_SESSION['user_id'])) {
         if ($_SESSION['role'] == 'admin') {
-            header("location:adminuser.php");
+            header("location:/adminuser.php");
             exit;
         } else {
-            header("location:student/home.php");
+            header("location:/student/home.php");
             exit;
         }
     }

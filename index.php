1a4d4014dd2bbdf7ee47047612027363debaedc3
sivavithu <sivavithu15@live.com>
2023-08-22 15:53:32 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,16 @@
-<?php echo "hello"; ?>
+<?php
+    session_start();
+    if(isset($_SESSION['user_id'])&& isset($_SESSION['role'])){
+        if($_SESSION['role']=='admin'){
+            header("location:adminuser.php");
+            exit;}
+    
+    else {
+        header("location:student/home.php");
+        exit;
+    }
+}
+else{
+    header("location:login.php");
+    exit;
+}?>

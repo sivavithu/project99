e21ae6498347f5b4072832dd89b66a3d081c2b3c
sivavithu <sivavithu15@live.com>
2023-08-23 10:25:30 +0530

    Update index.php

--- index.php
+++ index.php
@@ -13,11 +13,13 @@
 else{
     header("location:/login.php");
     exit;*/
-include ("/connection.php");
-$sql="select * from users where user_id=6";
-$result=mysqli_query($con,$sql);
-if($result){$row=mysqli_fetch_assoc($result);}
-else {echo "error";}
-
-echo $row['id'];
+include("/connection.php");
+$sql = "select * from users where user_id=6";
+$result = mysqli_query($con, $sql);
+if ($result) {
+    $row = mysqli_fetch_assoc($result);
+    echo $row['user_id']; // Assuming 'id' is a valid column name in your users table
+} else {
+    echo "error";
+}
 }?>

379a86017f0d49910d0a5b5b5538c2d02e2e23bd
sivavithu <sivavithu15@live.com>
2023-08-25 08:23:28 +0530

    Update createuser.php

--- admin/createuser.php
+++ admin/createuser.php
@@ -3,7 +3,7 @@ ob_start();
 session_start();
 
 
-if(!(isset($_SESSION['user_id']) && isset($_SESSION['role']) && $_SESSION['role'] == 'student')) {
+if(!(isset($_SESSION['user_id']) && isset($_SESSION['role']) && $_SESSION['role'] == 'admin')) {
      header("location:../login.php");
        exit;}
     

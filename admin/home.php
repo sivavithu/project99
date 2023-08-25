08cc8c8cc08024c786ca27d56be20cde78b3fb12
sivavithu <sivavithu15@live.com>
2023-08-25 08:23:17 +0530

    Update home.php

--- admin/home.php
+++ admin/home.php
@@ -5,7 +5,7 @@ ob_start();
 session_start();
 
 
-if(!(isset($_SESSION['user_id']) && isset($_SESSION['role']) && $_SESSION['role'] == 'student')) {
+if(!(isset($_SESSION['user_id']) && isset($_SESSION['role']) && $_SESSION['role'] == 'admin')) {
      echo "sss";
      header("location:../login.php");
        exit;

3a7ba1a43007b9d0404b4c692bcb38e68780368f
sivavithu <sivavithu15@live.com>
2023-08-25 07:56:24 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,16 @@
-<?php 
-  
-    header("location:/login.php");
+<?php
+    session_start();
+    if(isset($_SESSION['user_id'])&& isset($_SESSION['role'])){
+        if($_SESSION['role']=='admin'){
+            header("location:/admin/home.php");
+            exit;}
+    
+    else {
+        header("location:/student/home.php");
+        exit;
+    }
+}
+else{
+    header("location:login.php");
     exit;
-/*include("connection.php");
-$sql = "select * from users where user_id=6";
-$result = mysqli_query($con, $sql);
-if ($result) {
-    $row = mysqli_fetch_assoc($result);
-    echo $row['user_id']; // Assuming 'id' is a valid column name in your users table
-} else {
-    echo "error";
-}*/
-
-?>
+}?>

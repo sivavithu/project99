d9573bcdc315fb850c00d9ab4175cbfd848b2438
sivavithu <sivavithu15@live.com>
2023-08-25 13:49:38 +0530

    Update home.php

--- admin/home.php
+++ admin/home.php
@@ -3,16 +3,13 @@
 
 ob_start(); ?>
 <?php 
-error_reporting(E_ALL); // Report all types of errors
-ini_set('display_errors', 1); // Display errors on the screen
-
 session_start();
-$_SESSION['color'] = 'red';
+
 echo $_SESSION['color'];
-echo "hi", $_SESSION['user_id'];
+
 print_r($_SESSION);
 if (!(isset($_SESSION['user_id']) && isset($_SESSION['role']) && $_SESSION['role'] == 'admin')) {
-    echo "sss";
+
     header("location:../login.php");
     ob_end_flush();
 }
@@ -37,18 +34,4 @@ if (!(isset($_SESSION['user_id']) && isset($_SESSION['role']) && $_SESSION['role
 
 
  
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
 
-<body>
-     <h1>hi admin</h1>
-    <a href="/manageusers.php"><button>manage users</button></a>
-    
-</body>
-</html>

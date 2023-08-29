641e307458c65d3f71cc9d43ac2e93d8a8b202c2
sivavithu <sivavithu15@live.com>
2023-08-29 14:38:06 +0530

    Update login.php

--- login.php
+++ login.php
@@ -37,46 +37,49 @@
     </script>
 </head>
    </head>
-   <body>
-    <center><h1 class = "bh">COMPLAINT MANAGEMENT SYSTEM</h1></center>
-      <div class="login">
-         <form action="" class="login__form">
+  <body>
+    <center><h1 class="bh">COMPLAINT MANAGEMENT SYSTEM</h1></center>
+    <div class="login">
+        <form action="" class="login__form" method="post">
             <h1 class="login__title">Login</h1>
 
             <div class="login__content">
-               <div class="login__box">
-                  <i class="ri-user-3-line login__icon"></i>
+                <div class="login__box">
+                    <i class="ri-user-3-line login__icon"></i>
+                    <div class="login__box-input">
+                        <input type="username" name="username" required class="login__input" placeholder=" ">
+                        <label for="" class="login__label">Username</label>
+                    </div>
+                </div>
 
-                  <div class="login__box-input">
-                     <input type="username" required class="login__input" placeholder=" ">
-                     <label for="" class="login__label">Username</label>
-                  </div>
-               </div>
-
-               <div class="login__box">
-                  <i class="ri-lock-2-line login__icon"></i>
-
-                  <div class="login__box-input">
-                     <input type="password" required class="login__input" id="login-pass" placeholder=" ">
-                     <label for="" class="login__label">Password</label>
-                     
-                  </div>
-               </div>
+                <div class="login__box">
+                    <i class="ri-lock-2-line login__icon"></i>
+                    <div class="login__box-input">
+                        <input type="password" name="password" required class="login__input" id="login-pass" placeholder=" ">
+                        <label for="" class="login__label">Password</label>
+                    </div>
+                </div>
             </div>
 
             <div class="login__check">
-               <div class="login__check-group">
-                  <input type="checkbox" class="login__check-input">
-                  <label for="" class="login__check-label">Remember me</label>
-               </div>
-
-               <a href="otp.php" class="login__forgot">Forgot Password?</a>
+                <div class="login__check-group">
+                    <input type="checkbox" class="login__check-input">
+                    <label for="" class="login__check-label">Remember me</label>
+                </div>
+                <a href="otp.php" class="login__forgot">Forgot Password?</a>
             </div>
 
-            <button type="submit" class="login__button">Login</button>
+            <button type="submit" class="login__button" name="login">Login</button>
+        </form>
+    </div>
 
-         </form>
-      </div>
+    <?php
+    if (isset($_SESSION['error'])) {
+        echo "<div id='error-message'>" . $_SESSION['error'] . "</div>";
+        unset($_SESSION['error']);
+    }
+    ?>
+</body>
 
       <?php
     session_start();

400e731cb855fc960ffcfcc27105d77f5c7a2fcd
sivavithu <sivavithu15@live.com>
2023-09-02 13:32:33 +0530

    Update home.php

--- student/home.php
+++ student/home.php
@@ -37,7 +37,7 @@ if ($result && mysqli_num_rows($result) > 0) {
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-        <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="/css/home.css">
         <link href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css" rel="stylesheet">
         <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
         <script src="https://kit.fontawesome.com/b99e675b6e.js"></script>
@@ -55,32 +55,15 @@ if ($result && mysqli_num_rows($result) > 0) {
     window.history.replaceState(null,null,window.location.href);}
   
 
-        document.addEventListener("DOMContentLoaded", function() {
-            
-            const profileElementx = document.getElementById("profile2");
-            const imagePath = "<?php echo $imagePath; ?>"; 
-            
+    document.addEventListener("DOMContentLoaded", function() {
+    const profileImageElement = document.getElementById("profile");
+    const imagePath = "<?php echo $imagePath; ?>"; 
 
-        
-            profileElementx.style.backgroundImage = `url(${imagePath})`;
-            profileElementx.style.backgroundSize = "60px 60px"; // Set dimensions here
-        });
+    profileImageElement.src = imagePath;
+});
 
 </script>
-<style>
-	#profile2 {
-	border: 1px solid black;
-	height: 60px;
-	width: 60px;
-	margin: 10px;
-	border-radius: 50%; /* Set the border-radius to half of the width/height for a full circle */
-	box-shadow: 2px 3px 10px black;
-	background-color: white; /* Set a background color */
-	background-size: 100%; /* Adjust the background size to make the image smaller */
-	background-position: center; /* Center the background image */
-	
-}
-</style>
+
    
         <title>CMS</title>
         <link rel="icon" href="f.png" sizes="120x120" type="image/png">
@@ -118,7 +101,7 @@ if ($result && mysqli_num_rows($result) > 0) {
                     <div class="sidebar__inner">
                         <div class="profile">
                             <div class="img">
-                            <div id="profile2"></div>
+                                <img id="profile" src="" alt="profile_pic">
                             </div>
                             <div class="profile_info">
                                 <p>Welcome</p>

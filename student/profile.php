08320b83ceea0941d7f61dac7da81a914a98625d
sivavithu <sivavithu15@live.com>
2023-09-02 17:33:09 +0530

    Update profile.php

--- student/profile.php
+++ student/profile.php
@@ -97,7 +97,7 @@ if ($result && mysqli_num_rows($result) > 0) {
         <link href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css" rel="stylesheet">
         <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
         <script src="https://kit.fontawesome.com/b99e675b6e.js"></script>
-        <script src="https://kit.fontawesome.com/b99e675b6e.js"></script>
+        
         <script src="script.js"></script>
         <script src="https://code.jquery.com/jquery-3.6.0"> </script>
 
